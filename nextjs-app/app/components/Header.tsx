ea04ae542ea27fc306eff0007cda18d3a6ca1064
aramstepanian <132067726+aramstepanian@users.noreply.github.com>
2024-11-22 21:44:47 -0800

    Update Header.tsx

--- nextjs-app/app/components/Header.tsx
+++ nextjs-app/app/components/Header.tsx
@@ -32,7 +32,7 @@ export default function Header() {
                 >
                   <Link
                     href={item.href}
-                    className={item.className || ""}
+                    className={item.className}
                     {...(item.isExternal && {
                       target: "_blank",
                       rel: "noopener noreferrer",
